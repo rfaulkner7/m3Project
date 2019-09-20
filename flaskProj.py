598c6257b2dcf8b1df143fa4e683a2cb9233d8f6
Dohun <dyeo8@gatech.edu>
2019-09-21 06:29:31 +0900

    Merge branch 'Dohun'
    # Conflicts:
    #	flaskProj.py
    #	templates/home.html

--- flaskProj.py
+++ flaskProj.py
@@ -5,9 +5,19 @@ from wtforms import StringField, SubmitField, SelectField
 app = Flask(__name__)
 app.config['WTF_CSRF_ENABLED'] = False
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html', form = form)
+    form = HomeForm()
+    if form.is_submitted():
+        return redirect("http://127.0.0.1:8080/init", code=302)
+    return render_template('home.html', form=form)
+
+
+class HomeForm(FlaskForm):
+    input_field = StringField('Input')
+    submit = SubmitField('Start')
+
 
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1', port = 8080, debug = True)
+    app.run(host='127.0.0.1', port=8080, debug=True)

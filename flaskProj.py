c78e246cf8512898c0858bd38bd153602cb07485
Soph Kim <sophaejkim@users.noreply.github.com>
2019-09-20 13:09:16 -0400

    Update flaskProj.py

--- flaskProj.py
+++ flaskProj.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, IntegerField
 
 app = Flask(__name__)
 app.config['WTF_CSRF_ENABLED'] = False
@@ -8,6 +8,9 @@ app.config['WTF_CSRF_ENABLED'] = False
 @app.route('/', methods=['GET','POST'])
 def home():
     return render_template('home.html', form = form)
+
+@app.route('/init', methods=['GET', 'POST'])
+def initialize():
 
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port = 8080, debug = True)

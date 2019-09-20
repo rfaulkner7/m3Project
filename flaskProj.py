993f9b6985bd3595267a35b440e74d2fda3ec089
rfaulkner7 <rfaulkner7@gatech.edu>
2019-09-20 11:21:33 -0400

    initial layout

---
+++ flaskProj.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+
+app = Flask(__name__)
+app.config['WTF_CSRF_ENABLED'] = False
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    return render_template('home.html', form = form)
+
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port = 8080, debug = True)
